3af4826d9fee9ec4d1063d695063e5a7f4bb883b
Anirudh Singh <anirudhisgreat1200@gmail.com>
2021-04-08 10:04:00 +0530

    fixed dark mode

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c80aef84c4e86491fc283dc981bc37db",
-"/": "c80aef84c4e86491fc283dc981bc37db",
-"main.dart.js": "2d560f7d361a0412b98abde963e5506b",
+"index.html": "9bbea6b02a560ac4d6cd2e9f239efe3e",
+"/": "9bbea6b02a560ac4d6cd2e9f239efe3e",
+"main.dart.js": "030ed86f37cc74d38759fa71c3f51f21",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
 };
