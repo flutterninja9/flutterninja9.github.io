bf012b367997f28933ac738690738c31d018619b
Anirudh Singh <anirudhisgreat1200@gmail.com>
2021-04-12 09:05:36 +0530

    fixed music support

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e860bf6483a43f4322d00af057d1b436",
-"/": "e860bf6483a43f4322d00af057d1b436",
-"main.dart.js": "f5110ba759b4ca0ab17eb28cc1d42377",
+"index.html": "7fad00e35bee6cd78ee601531b338469",
+"/": "7fad00e35bee6cd78ee601531b338469",
+"main.dart.js": "e2fd62411b64c4368e210a7cc9a366c6",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
 };
