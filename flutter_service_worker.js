3b54c347088ae80a3eece4e2600a85f063e8da0a
Anirudh Singh <anirudhisgreat1200@gmail.com>
2021-05-05 17:50:10 +0530

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c34de5423707a0404afbb0a5fa78bd60",
-"/": "c34de5423707a0404afbb0a5fa78bd60",
-"main.dart.js": "c1d18357b0aabf80aa01261842f4a591",
+"index.html": "d6462fc78acd7c19928e3ed789bf7151",
+"/": "d6462fc78acd7c19928e3ed789bf7151",
+"main.dart.js": "da9cbf4f774691f3ebc2ebb51c574367",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
 };
